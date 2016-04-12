92ab507e6288354c9c869d7c1c8b4739430b5da8
jean-michel-legrand <defactojml@gmail.com>
2016-04-12 06:54:34 +0200

    Add beforeEach statement

--- spec/AddressBookSpec.js
+++ spec/AddressBookSpec.js
@@ -1,17 +1,22 @@
 describe('Address Book', function(){
+
+  var addressBook;
+  var contact;
+
+  beforeEach(function(){
+    addressBook = new AddressBook();
+    contact = new Contact();
+  });
+
   it('should be able to add a contact', function() {
-    var addressBook = new AddressBook();
-    var contact = new Contact();
+
     addressBook.addBook(contact);
     expect(addressBook).not.toBeNull();
     expect(addressBook.getBook(0)).toEqual(contact);
   });
 
   it('should be able to delete a contact', function() {
-    var addressBook = new AddressBook();
-    var contact = new Contact();
     var contact2 = new Contact();
-
     addressBook.addBook(contact);
     addressBook.addBook(contact2);
     addressBook.delete(0);
